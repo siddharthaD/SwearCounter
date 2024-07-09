29208385fa3bb949f203b47b2339e24594e6ae6a
Sid D <siddhuiiti@hotmail.com>
2024-07-08 18:17:25 -0700

    chore: Added a common index file to list the different projects. Added vite config for each of the pages. Corrected link entry points

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { resolve } from 'path'
+
+export default {
+        build: {
+          rollupOptions: {
+            input: {
+              index: 'index.html',
+               swearcounter:'swearcounter.html',
+               mumbai: 'mumbai.html',
+               gamescore: 'gamescore.html',
+               agency:'DaveAgency.html',
+                birthdaysite:'birthdaysite.html',
+                businesscard:'businesscard.html',
+              passwordgenerator:'passwordgenerator.html',
+              productpage:'productpage.html',
+                nft: resolve(__dirname, 'NFT/index.html'),
+                cookieconset: resolve(__dirname,'cookieconsent/cookie.html'),
+                coworking: resolve(__dirname,'CoWorking/index.html')
+              //   nft:'./NFT/swearcounter.html'
+            }
+          }
+        }
+}
